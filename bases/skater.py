1ff9f5538140ccf1d86d929c050ad309432183e1
Eric Hansen <ehansen@securityfor.us>
2013-08-09 11:00:16 -0400

    Added more attributes and created the skater class (used for fwd's and d's)

---
+++ bases/skater.py
@@ -0,0 +1,31 @@
+from bases.player import Player as PB
+
+class Skater(PB):
+	# Offensive
+	deking = 50
+	hand_eye = 50
+	off_awareness = 50
+	puck_control = 50
+	slapshot_accuracy = 50
+	slapshot_power = 50
+	wristshot_accuracy = 50
+	wristshot_power = 50
+
+	# Defensive
+	aggressiveness = 50
+	body_checking = 50
+	def_awareness = 50
+	discipline = 50
+	faceoffs = 50
+	fighting_skill = 50
+	shot_blocking = 50
+	stick_checking = 50
+	blocking_accuracy = 50
+
+	# Athleticis
+	acceleration = 50
+	agility = 50
+	balance = 50
+	durability = 50
+	endurance = 50
+	strength = 50
